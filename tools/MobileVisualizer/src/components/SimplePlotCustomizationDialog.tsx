699a61c143cd24a74715810dfcdcf4407554ae87
benjaled <benjasumny@gmail.com>
2025-06-28 16:42:52 -0500

    Fix z-index stacking issue for full-screen viewer - use z-[9999] and add render debugging

--- tools/MobileVisualizer/src/components/SimplePlotCustomizationDialog.tsx
+++ tools/MobileVisualizer/src/components/SimplePlotCustomizationDialog.tsx
@@ -672,7 +672,8 @@ export function SimplePlotCustomizationDialog({
 
       {/* Full-Screen Image Viewer Modal */}
       {showFullImageViewer && (
-        <div className="fixed inset-0 bg-black bg-opacity-95 flex items-center justify-center z-70 p-4">
+        console.log('Rendering full-screen viewer'),
+        <div className="fixed inset-0 bg-black bg-opacity-95 flex items-center justify-center z-[9999] p-4">
           <div className="w-full h-full flex flex-col">
             {/* Modal Header */}
             <div className="flex items-center justify-between p-4 text-white">
